a3e2d9ea6bf53b964674dc209a72a8edc1164ade
PrabhakarG001 <Prabhakarg465@gmail.com>
2026-09-09 22:27:48 +0530

    chore: suppress THREE.Clock deprecation warning

--- prabhakar-gupta-portfolio/src/main.jsx
+++ prabhakar-gupta-portfolio/src/main.jsx
@@ -4,6 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+// Suppress THREE.Clock deprecation warning from libraries
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (typeof args[0] === "string" && args[0].includes("THREE.Clock: This module has been deprecated")) {
+    return;
+  }
+  originalWarn(...args);
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
